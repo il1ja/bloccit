e50bb570fe92ce6bad090cda0b583db268177d23
Travis Grossman <TravisGrossman@Traviss-iMac.local>
2018-02-27 14:46:27 -0800

    assignment seeding 3rd commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,39 +2,39 @@ require 'random_data'
 
 
 50.times do
-
   Post.create!(
-
     title:  RandomData.random_sentence,
     body:   RandomData.random_paragraph
   )
 end
 posts = Post.all
 
-
 100.times do
   Comment.create!(
-
     post: posts.sample,
     body: RandomData.random_paragraph
   )
 end
 
-10.times do
-  Comment.find_or_create_by!(
+Post.find_or_create_by(title: "First title", body: "A body")
+Post.find_or_create_by(title: "second title", body: "A second body")
+Post.find_or_create_by(title: "third title", body: "A third body")
 
-    post: posts.sample,
-    body: RandomData.random_paragraph
-  )
-end
-puts "#{Post.count}"
-5.times do
-  Post.find_or_create_by!(
-    title: RandomData.random_sentence,
-    body: RandomData.random_paragraph
-  )
-end
-puts "#{Post.count}"
+
+# 10.times do
+#   Comment.find_or_create_by!(
+#
+#     post: posts.sample,
+#     body: RandomData.random_paragraph
+#   )
+# end
+# puts "#{Post.count}"
+# 5.times do
+#   Post.find_or_create_by!(
+#     title: RandomData.random_sentence,
+#     body: RandomData.random_paragraph
+#   )
+# end
 
 
 puts "Seed finished"
