2855bdfc411421ea04a4e941bf0cd1765d714d3c
Travis Grossman <TravisGrossman@Traviss-iMac.local>
2018-03-05 15:27:20 -0800

    assignment validations

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,7 +1,7 @@
 class Topic < ApplicationRecord
   has_many  :posts, dependent: :destroy
-  
 
-  validates :title, length: { minimum: 5 }, presence: true
-  validates :body, length: { minimum: 20 }, presence: true
+
+  validates :name, length: { minimum: 5 }, presence: true
+  validates :description, length: { minimum: 15 }, presence: true
 end
