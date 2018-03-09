097a079c5285cd5231a5d04ba3aa7f2c7878765d
Travis Grossman <TravisGrossman@Traviss-iMac.local>
2018-03-09 13:42:21 -0800

    checkpoint sign in

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,19 @@
 class User < ApplicationRecord
-  before_save {self.email = email.downcase if email.present?}
-  before_save {self.name = self.name.split.map(&:capitalize).join(' ')}
+  has_many :posts
 
-  validates :name, length: {minimum: 1, maximum: 100},presence: true
+  before_save { self.email = email.downcase if email.present? }
 
-  validates :password, presence: true, length: { minimum: 6 }, if: "password_digest.nil?"
-  validates :password, length: { minimum: 6 }, allow_blank: true
+
+  validates :name, length: { minimum: 1, maximum: 100 }, presence: true
+
+ validates :password, presence: true, length: { minimum: 6 }, if: "password_digest.nil?"
+ validates :password, length: { minimum: 6 }, allow_blank: true
 
   validates :email,
-          presence: true,
-          uniqueness: { case_sensitive: false },
-          length: { minimum: 3, maximum: 254 }
+            presence: true,
+            uniqueness: { case_sensitive: false },
+            length: { minimum: 3, maximum: 254 }
 
-has_secure_password
+
+  # has_secure_password
 end
