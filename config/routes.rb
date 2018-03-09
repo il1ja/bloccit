73db206a467288cdefa734f6127b42a649da26a2
Travis Grossman <TravisGrossman@Traviss-iMac.local>
2018-03-09 10:41:05 -0800

    assignment authentication

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,12 @@ Rails.application.routes.draw do
     resources :sponsored_posts, except: [:index]
   end
   resources :users, only: [:new, :create]
-
   resources :questions
 
+  post "users/confirm" => "users#confirm"
+
   get 'about' => 'welcome#about'
+
   root 'welcome#index'
 
 end
