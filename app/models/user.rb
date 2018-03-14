df8b9ed88df061be861d73178a953a3105f6460c
Travis Grossman <TravisGrossman@Traviss-iMac.local>
2018-03-14 13:53:48 -0700

    checkpoint comments

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,11 @@
 class User < ApplicationRecord
 has_many :posts, dependent: :destroy
-
+has_many :comments, dependent: :destroy
 
   before_save { self.email = email.downcase if email.present? }
   before_save { self.role ||= :member }
 
-
-  validates :name, length: { minimum: 1, maximum: 100 }, presence: true
-
+ validates :name, length: { minimum: 1, maximum: 100 }, presence: true
  validates :password, presence: true, length: { minimum: 6 }, if: "password_digest.nil?"
  validates :password, length: { minimum: 6 }, allow_blank: true
 
@@ -15,7 +13,6 @@ has_many :posts, dependent: :destroy
             presence: true,
             uniqueness: { case_sensitive: false },
             length: { minimum: 3, maximum: 254 }
-
 
   has_secure_password
 
