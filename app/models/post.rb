7b2d3090613239f692fc398c73bb26de39edeaf2
Travis <Travisagrossman@gmail.com>
2018-03-16 17:08:36 -0700

    editing specs and models for favorites

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,8 @@ class Post < ApplicationRecord
 
   has_many :comments, dependent: :destroy
   has_many :votes, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+
   validates :title, length: { minimum: 5 }, presence: true
   validates :body, length: { minimum: 20 }, presence: true
   validates :topic, presence: true
