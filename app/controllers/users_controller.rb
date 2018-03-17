00225356958969e95f7093eab48b8adfad67b0c3
Travis <Travisagrossman@gmail.com>
2018-03-16 20:19:31 -0700

    issue adding sendgrid addon

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,8 +7,8 @@ class UsersController < ApplicationController
     @user = User.new
     @user.name = params[:user][:name]
     @user.email = params[:user][:email]
-    @user.password = params [:user][:password]
-    @user.password_confirmation = params [:user][:password_confirmation]
+    @user.password = params[:user][:password]
+    @user.password_confirmation = params[:user][:password_confirmation]
 
     if @user.save
       flash[:notice] = "Welcome to Bloccit #{@user.name}!"
